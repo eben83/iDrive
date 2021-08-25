eb7eee0924a484f2024ff54f47183ed36dbd6566
Eben Burger <eben@arlowconsulting.co.za>
2021-08-25 13:19:32 +0200

    added react-slick slider

--- idrive/idrive.WebClient/ClientApp/components/imageSlider.js
+++ idrive/idrive.WebClient/ClientApp/components/imageSlider.js
@@ -1,137 +1,100 @@
-/*import React, { useState } from "react";
-import Image from "next/image";
-
-const Carousel = () => {
-    const [current, setCurrent] = useState(0)
-    
-    const images = [
-        {
-            image: '../images/002.jpg',
-            altText: "Slide 1",
-            caption: "",
-            header: "",
-            key: "1",
-        },
-        {
-            image: '../images/014.jpg',
-            altText: "Slide 2",
-            caption: "",
-            header: "",
-            key: "2",
-        },
-        {
-            image: '../images/007.jpg',
-            altText: "Slide 3",
-            caption: "",
-            header: "",
-            key: "3",
-            width: 'min-w-full'
-        },
-    ]
-    
-    return (
-        <>
-            <div className='flex relative w-screen h-64'>
-                {images.map ((slide, index) => {
-                    return <img className='w-screen object-cover justify-content-center'
-                                src={slide.image}
-                                alt={slide.altText}
-                    />
-                })}
-            </div>
-        </>
-    )
-}
-export default Carousel;*/
-
-import React, { useState } from 'react';
-import {
-    Carousel,
-    CarouselItem,
-    CarouselControl,
-    CarouselIndicators,
-    CarouselCaption
-} from 'reactstrap';
-
-const items = [
-    {
-        id: 1,
-        img: '../../images/002.jpg',
-        altText: 'Slide 1',
-        caption: 'Slide 1'
-    },
-    {
-        id: 2,
-        altText: 'Slide 2',
-        caption: 'Slide 2'
-    },
-    {
-        id: 3,
-        altText: 'Slide 3',
-        caption: 'Slide 3'
-    }
-];
+import React, {} from 'react';
+import styled from "styled-components";
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 
 const ImageSlider = (props) => {
-    const [activeIndex, setActiveIndex] = useState(0);
-    const [animating, setAnimating] = useState(false);
 
-    const next = () => {
-        if (animating) return;
-        const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
-        setActiveIndex(nextIndex);
+    let settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
     }
-
-    const previous = () => {
-        if (animating) return;
-        const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
-        setActiveIndex(nextIndex);
-    }
-
-    const goToIndex = (newIndex) => {
-        if (animating) return;
-        setActiveIndex(newIndex);
-    }
-
-    const slides = items.map((item) => {
-        return (
-            <CarouselItem
-                className="custom-tag"
-                tag="div"
-                key={item.id}
-                onExiting={() => setAnimating(true)}
-                onExited={() => setAnimating(false)}
-            >
-                <CarouselCaption className="text-danger" captionText={item.caption} captionHeader={item.caption} />
-            </CarouselItem>
-        );
-    });
-
     return (
-        <div>
-            <style>
-                {
-                    `.custom-tag {
-              max-width: 100%;
-              height: 500px;
-              background: black;
-            }`
-                }
-            </style>
-            <Carousel
-                activeIndex={activeIndex}
-                next={next}
-                previous={previous}
-            >
-                <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
-                {slides}
-                <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
-                <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+        <>
+            <Carousel {...settings}>
+                <Wrap>
+                    <a>
+                        <img src='/images/002.jpg' alt='image' />
+                    </a>
+                </Wrap>
+                <Wrap>
+                    <a>
+                        <img src='/images/007.jpg' alt='image' />
+                    </a>
+                </Wrap>
+                <Wrap>
+                    <a>
+                        <img src='/images/014.jpg' alt='image' />
+                    </a>
+                </Wrap>
             </Carousel>
-        </div>
+        </>
     );
 }
+const Carousel = styled(Slider)`
+    
+    & > button {
+        opacity: 0;
+        height: 100%;
+        width: 5vw;
+        z-index: 1;
+        
+        &:hover {
+            opacity: 1;
+            transition: opacity 0.2s ease 0s
+        }
+    }
+    
+    ul li button {
+        &:before {
+            font-size: 10px;
+            color: rgb(150,158,171);
+        }
+    }
+    
+    .slick-list {
+        overflow: initial;
+    }
+    
+    .slick-prev {
+        left: 1rem;
+    }
+    
+    .slick-next {
+        right: 1rem;
+    }
+`;
 
+const Wrap = styled.div`
+    border-radius: 4px;
+    cursor: pointer;
+    position: relative;
+    
+    a {
+        border-radius: 4px;
+        box-shadow: rgb (0 0 0 / 69%) 0 26px 30px -10px,
+                    rgb (0 0 0 / 73%) 0 16px 10px -10px;
+        cursor: pointer;
+        display: block;
+        position: relative;
+        padding: 4px;
+        
+        img {
+            width: 100%;
+            height: 42vh;
+            object-fit: cover
+        }
+        
+        &:hover {
+            padding: 0;
+            border: 4px solid rgba(249,249,249,0.8);
+            transition-duration: 300ms;
+        }
+    }
+`;
 export default ImageSlider;
-
-
