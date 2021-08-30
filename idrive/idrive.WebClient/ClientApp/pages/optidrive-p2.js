adcc9a89d9fcdf6a20a0deade7e91c155166c304
Eben Burger <eben@arlowconsulting.co.za>
2021-08-30 15:34:20 +0200

    optidrive p2 page complete

--- idrive/idrive.WebClient/ClientApp/pages/optidrive-p2.js
+++ idrive/idrive.WebClient/ClientApp/pages/optidrive-p2.js
@@ -1,5 +1,7 @@
 ﻿import React, {} from 'react';
 import Nav from '../components/nav'
+import Footer from '../components/footer'
+import ScrollToTop from '../components/scrollToTop'
 
 const OptidriveP2 = (props) => {
     return (
@@ -172,28 +174,28 @@ const OptidriveP2 = (props) => {
                 </div>
             </div>
             
-            <div className=''>
+            <div className='lg:container lg:mx-auto lg:flex lg:flex-row-reverse'>
                 <img src='./images/eco-features.jpg' alt='p3 features' />
-                <div className='flex justify-center px-4 flex-wrap text-center'>
-                    <div className='mt-6 mb-6 w-1/2'>
+                <div className='flex justify-center px-4 flex-wrap text-center lg:text-left lg:flex-col '>
+                    <div className='mt-6 mb-6 w-1/2 lg:w-max'>
                         <div className='text-indigo-500 text-3xl font-bold '>IP20</div>
                         <p className='text-gray-500'>
                             Sizes 2-3; up to 11kW
                         </p>
                     </div>
-                    <div className='mt-6 mb-6 w-1/2'>
+                    <div className='mt-6 mb-6 w-1/2 lg:w-max'>
                         <div className='text-indigo-500 text-3xl font-bold '>IP55</div>
                         <p className='text-gray-500'>
                             Size 8; up to 250kW
                         </p>
                     </div>
-                    <div className='mt-6 mb-6 w-1/2'>
+                    <div className='mt-6 mb-6 w-1/2 lg:w-max'>
                         <div className='text-indigo-500 text-3xl font-bold '>IP55</div>
                         <p className='text-gray-500'>
                             Sizes 4-7; up to 160kW
                         </p>
                     </div>
-                    <div className='mt-6 mb-6 w-1/2'>
+                    <div className='mt-6 mb-6 w-1/2 lg:w-max'>
                         <div className='text-indigo-500 text-3xl font-bold '>IP66</div>
                         <p className='text-gray-500'>
                             Sizes 2-3; up to 7.5kW;
@@ -204,6 +206,154 @@ const OptidriveP2 = (props) => {
                     </div>
                 </div>
             </div>
+
+            <div className='w-auto bg-black h-px md:mx-32 lg:container lg:mx-auto ' />
+            
+            <div className='px-4 mt-12 mb-12 flex flex-col lg:flex-row items-center lg:justify-evenly lg:container lg:mx-auto'>
+                <img src='./images/safe-torque-off.jpg' alt='with and without image' />
+                <div className='lg:w-1/2'>
+                    <div className='text-2xl text-indigo-500 font-bold pt-5'>
+                        Safe Torque Off
+                    </div>
+                    <p className='text-gray-500 pt-5'>
+                        With and without safe torque off Optidrive P2 features a safe torque off function to allow 
+                        simple integration into machine critical safety circuits.
+                    </p>
+                    <ul className='list-disc inner-list text-gray-500 px-5 pt-5'>
+                        <li>
+                            Simple machine design reduces component costs, saves
+                            panel space and minimises installation time.
+                        </li>
+                        <li>
+                            Faster shut down and reset procedures reduce system
+                            maintenance time
+                        </li>
+                        <li>
+                            Better safety standard compared to mechanical solution.
+                        </li>
+                        <li>
+                            Better motor connection. Single cable with no interruption.
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            
+            <div className='bg-gray-200 text-center pt-12 pb-12 mt-12 mb-12 px-6 md:px-24'>
+                <div className='lg:container lg:mx-auto'>
+                    <div className='text-white text-5xl '>
+                        Powerful, versatile and easy to use
+                    </div>
+                    <div>
+                        <div className='font-bold text-indigo-500 pt-10'>
+                            Advanced Motor Control
+                        </div>
+                        <p className='pt-5 text-gray-600'>
+                            The P2 Variable Frequency Drive has been uniquely developed to allow a wide range of different
+                            motor types to be used, with only parameter changes being required. This technology allows the
+                            same drive to be used in a wide range of applications, allowing OEM's and end user alike to take
+                            advantage of the energy saving provided by using the latest motor technologies.
+                        </p>
+                    </div>
+                    <div>
+                        <div className='font-bold text-indigo-500 pt-10'>
+                            AC Induction Motors
+                        </div>
+                        <p className='pt-5 text-gray-600'>
+                            The majority of AC motors in use today around the world are standard induction motors. These motors are relatively low cost, readily available and provide good performance with long service life. With the ever increasing focus on energy efficiency, motor manufacturers have refined and improved their designs in recent years.
+                        </p>
+                        <p className='pt-5 text-gray-600'>
+                            Optidrive P2 has been developed to provide optimum control and maximum efficiency when 
+                            operating with older motors designs, or newer high efficiency designs.
+
+                            Operation can be in simple V/F control mode or in High Performance Third Generation Vector 
+                            Mode, which provides up to 200% torque from zero speed without requiring an encoder.
+                        </p>
+                    </div>
+                    <div>
+                        <div className='font-bold text-indigo-500 pt-10'>
+                            Permanent Magnet AC Motors
+                        </div>
+                        <p className='pt-5 text-gray-600'>
+                            Permanent magnet AC motors provide improved efficiency compared to standard induction motors.
+                            Using permanent magnets in the motor construction eliminates the need for any magnetising 
+                            current, reducing electrical losses. PM motors have been used for many years in high 
+                            performance applications, however this has always required the use of a feedback device, 
+                            such as a resolver or encoder. Optidrive P2 has been designed to operate with AC PM motors 
+                            without requiring any feedback device, allowing them to be used for their energy efficiency 
+                            benefits without incurring extra cost and complexity in applications which do not require 
+                            position feedback.
+                        </p>
+                    </div>
+                    <div>
+                        <div className='font-bold text-indigo-500 pt-10'>
+                            Brushless DC Motors
+                        </div>
+                        <p className='pt-5 text-gray-600'>
+                            BLDC motors are similar to AC PM motors, however the design requires a slightly different 
+                            control method to optimise the performance. Optidrive P2 has the flexibility to control this
+                            type of motor, requiring only simple parameter changes. This provides much greater 
+                            flexibility for OEM's, allowing Optidrive P2 to be used in a variety of applications, with 
+                            various motor types.
+                        </p>
+                    </div>
+                    <div>
+                        <div className='font-bold text-indigo-500 pt-10'>
+                            Synchronous Reluctance Motors
+                        </div>
+                        <p className='pt-5 text-gray-600'>
+                            Synchronous Reluctance Motors (SynRM), not to be confused with Switched Reluctance Motors, 
+                            share a similar stator construction to standard induction motors, however the rotor is 
+                            substantially different, in order to improve the overall efficiency of the motor. SynRM 
+                            motors are ideally suited to variable torque applications.
+                        </p>
+                        <p className='pt-5 text-gray-600'>
+                            Optidrive P2 can control synchronous reluctance motors, allowing the energy saving benefits
+                            to be realised.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            
+            <div className='flex flex-col md:flex-row px-5 lg:container lg:mx-auto'>
+                <div className=' md:w-1/2 md:p-6 lg:pb-0'>
+                    <img src='./images/optidrive-p2-group-22-32-right-group.jpg' alt='p2 group image' />
+                </div>
+                <div className='pt-6 md:w-1/2 md:pb-6'>
+                    <div className='font-bold text-indigo-500'>Simple PLC Functionality</div>
+                    <p className='text-gray-500 pt-6 md:pt-0'>
+                        A wide range of function types available including:
+                    </p>
+                    <ul className='list-disc inner-list text-gray-500'>
+                        <li>
+                            Programmable logic functions
+                        </li>
+                        <li>
+                            Comparators
+                        </li>
+                        <li>
+                            Timers
+                        </li>
+                        <li>
+                            Mathematical functions
+                        </li>
+                        <li>
+                            Drive specific functions
+                        </li>
+                        <li>
+                            All blocks can be easily combined to create flexible programs.
+                        </li>
+                    </ul>
+                    <p className='text-gray-500'>
+                        Programs can be protected to prevent unauthorised copying.
+                    </p>
+                    <p className='text-gray-500'>
+                        Complete control over the drive including all inputs and outputs.
+                    </p>
+                </div>
+            </div>
+            
+            <Footer />
+            <ScrollToTop />
         </>
     );
 }
